eb2032284b0818fadbc1dae61d7ddc80d24f0eba
Martin Killenberg <martin.killenberg@desy.de>
2017-06-12 12:58:42 +0200

    took out the restriction not to allow read/write access to BAR 2. This decision does not belong to the base driver

--- pcieuni_rw_no_struct_exp.c
+++ pcieuni_rw_no_struct_exp.c
@@ -65,16 +65,6 @@ int checkAndCalculateTransferInformation( pcieuni_dev const * deviceData,
 	    transferInformation->offset);  
   */
 
-  /* Do not allow access to the DMA bar with normal read/write operations from
-   * user space. This is a security issue to avoid DMA injections. Only the driver is
-   * allowed to transfer the DMA target address to the hardware.
-   */
-  if (transferInformation->bar == PCIEUNI_DMA_BAR){
-      printk("PCIEUNI_TRANSFER_INFO_CHECK: Bar %d reserved for DMA. No direct access allowed.\n",
-	     PCIEUNI_DMA_BAR);
-      return -EFAULT;
-  }
-
   /* get the bar's start and end address */
   /* FIXME: organise the information as arrays, not as individual variables, and you might get rid of this block */
   switch (transferInformation->bar){
