94bfa7d6dc37c16e4362d8538d92a931f2ed18fb
Martin Killenberg <martin.killenberg@desy.de>
2018-07-11 14:36:50 +0200

    adapted script to use (local) git repo instead of svn

--- make_debian_package.sh
+++ make_debian_package.sh
@@ -28,7 +28,7 @@ CHECKOUT_DIRECTORY=${PWD}/debian_package/${GPCIEUNI_PACKAGE_VERSION}
 
 rm -rf debian_package
 mkdir debian_package
-svn co https://svnsrv.desy.de/public/mtca4u/drivers/gpcieuni/tags/${GPCIEUNI_PACKAGE_VERSION} ${CHECKOUT_DIRECTORY}
+git clone . --branch ${GPCIEUNI_PACKAGE_VERSION} --single-branch ${CHECKOUT_DIRECTORY}
 
 cp -r debian_from_template ${CHECKOUT_DIRECTORY}/debian
 
